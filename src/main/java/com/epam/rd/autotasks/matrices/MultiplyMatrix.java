fe6edf312278ebf5715a28db454958cc53d46e1c
Zapletin Daniil <Zaplot1994@gmail.com>
2022-12-23 22:49:12 +0300

    Task done with all test passed

--- src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -3,26 +3,32 @@ import java.util.Arrays;
 
 public class MultiplyMatrix {
     public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        int [][]result = new int[matrix1.length][matrix2[0].length];
+        for (int i=0; i< matrix1.length; i++) {
+            for (int j=0; j< matrix2[0].length; j++) {
+                for (int k=0; k< matrix2.length; k++) {
 
-        // Put your code here
-
-        return null;
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Test your code here!\n");
+        //System.out.println("Test your code here!\n");
 
         // Get a result of your code
 
         int[][] a = {
-                {0, 12345},
-                {4509, 0},
-                {3, 567} };
+                {1, 2, 3},
+                {4, 5, 6} };
 
         int[][] b = {
-                {653, 0, 25353},
-                {0, 61, 6} };
+                {7, 8},
+                {9, 10},
+                {11, 12} };
 
         int[][] result = multiply(a, b);
         System.out.println(Arrays.deepToString(result).replace("],", "]\n"));
